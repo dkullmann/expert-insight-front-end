ccf040d0e7feb10966708104890921acbfb1aefc
Andi Farr <andi@semibad.com>
2011-10-10 16:27:37 +0100

    week 1 pages complete
    fixed js bug (absolute path issue)

--- js/main.js
+++ js/main.js
@@ -13,5 +13,33 @@ $(document).ready(function() {
 		});
 	});
 	
+	// scroller
+	$(".scroller").each(function() {
+		$(this).append($("<a class='prev'>Previous</a>")).append($("<a class='next'>Next</a>"));
+	});
+	
+	// experts list
+	$("#experts-list").each(function() {
+		var tabs = $(this).children('ul');
+		var count = tabs.length;
+		if (count < 2) { return; }
+		// set up controls
+		var controls = $("<ol class='controls' />");
+		for (var i = 1; i <= count; i++) {
+			$("<li />").text(i).appendTo(controls);
+		}
+		controls.children('li:first-child').addClass('current');
+		controls.appendTo($(this).parent());
+		// bind 'em
+		controls.children('li').click(function() {
+			var target = $(tabs[$(this).index()])
+			tabs.css('zIndex', 0);
+			target.hide().css('zIndex', 1).fadeIn('fast');
+			controls.children('li').attr('class', '');
+			$(this).addClass('current');
+		});
+		
+	});
+	
 	
 });
