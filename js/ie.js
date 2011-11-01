a4cda33c5dd7a56703f3b8f95ccf7a2e2b7d8872
Andi Farr <andi@semibad.com>
2011-11-01 00:54:28 +0000

    search placeholder text
    added placeholder attribute to search field on index.html
    added /js/ie.js to polyfill missing functionality for ie7/8 – includes
    placeholder polyfill

---
+++ js/ie.js
@@ -0,0 +1,26 @@
+// placeholder fix
+(function($) {
+	$(function() {
+		if(!('placeholder' in document.createElement("input"))) { //don't do any work if we don't have to!
+			var inputs = $("input[type='text']"),
+				origColour = inputs.eq(0).css("color");
+
+			inputs
+				.each(function(i) {
+					var $t = $(this);
+					$t.val($t.attr("placeholder")).css("color", "#585858");
+				})
+				.focus(function() {
+					var $t = $(this);
+					if($t.val() === $t.attr("placeholder")) $t.val('').css("color", origColour);
+				})
+				.blur(function() {
+					var $t = $(this);
+					if($t.val() === '') {
+						$t.val($t.attr("placeholder")).css("color", "#585858");
+					} 
+				})
+		}
+	})
+
+})(jQuery);
