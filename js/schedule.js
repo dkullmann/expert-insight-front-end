0429174ae3b4806ed2104bd7d8e629b003619590
Andi Farr <andi@semibad.com>
2011-11-28 11:35:42 +0000

    weekend changes

--- js/schedule.js
+++ js/schedule.js
@@ -65,4 +65,78 @@ $(document).ready(function() {
 		e.preventDefault();
 	});
 	
+	// weekly template
+	$('#week .add-block').click(function() {
+		var list = $(this).parent().children('ol');
+		var clone = list.children('li:first').clone();
+		clone.appendTo(list);
+	});
+	$('#week').delegate('select', 'focus', function(e) {
+		var type = $(this).attr('class');
+		var day = $(this).closest('ol');
+		$('option', this).remove();
+		// fill the list
+		for (var i=0; i<24; i++) {
+			var hr = (i<10) ? '0'+i : i;
+			$(this).append("<option value='" + hr + "00'>" + i + ":00</option><option value='" + hr + "30'>" + i + ":30</option>");
+		}
+		// remove any that are invalid
+		var that = this;
+		$('li', day).each(function() {
+			if (this != $(that).parent()) {
+				var from = parseInt($('select.from', this).val());
+				var until = parseInt($('select.until', this).val());
+				$('option', that).each(function() {
+					if (parseInt($(this).attr('val')) > until) {
+						return false;
+					}
+					if (parseInt($(this).attr('val')) >= from) {
+						$(this).remove();
+					}
+				});
+			}
+						
+		});
+		
+	}).delegate('select', 'change', function(e) {
+		// reorder the elements
+		var item = $(this).parent();
+		var this_time = $('select.from', item).val();
+		var before = false;
+		$('select.from', item.parent()).each(function() {
+			if ($(this).val() > this_time) {
+				before = $(this).parent();
+				return false;
+			}
+		});
+		if (before) {
+			item.insertBefore(before);
+		} else {
+			item.appendTo(item.parent());
+		}
+	});
+	
+	// weekly template (alt)
+	var context = 'add';
+	$('#week-grid').selectable({
+		stop: function() {
+			$( ".ui-selected", this ).each(function() {
+				if (context == 'add') {
+					$(this).addClass('available');
+				} else {
+					$(this).removeClass('available');
+				}
+			});
+		}
+	});
+	$('#week-grid li li').click(function() {
+		$(this).toggleClass('available');
+	});
+	$('#template .controls a').click(function() {
+		$('#template .controls a').removeClass('active');
+		context = $(this).attr('class');
+		$(this).addClass('active');
+	});
+	
+	
 });
